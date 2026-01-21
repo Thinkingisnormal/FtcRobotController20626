306d89ec41d19f930270f4bdd0fe1b6990803f55
Matthew <busfan750@gmail.com>
2026-01-20 20:46:02 -0800

    Beginning to progress....

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -1,7 +1,36 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.controller.PIDFController;
 
+@Configurable
 public class Shooter extends SubsystemBase {
+    public enum ShooterMode{
+        RAW, PIDF,
+        //FIXED,
+    }
+
+    private ShooterMode shooterMode = ShooterMode.RAW;
+
+    //Raw Power Mode
+    private double power = 0.0;
+    public static double SHOOT_POWER = 0.1;
+
+    //PIDF Mode
+    private double targetVelcity = 0.0;
+    private PIDFController flywheelPID = new PIDFController(0,0,0,0);
+
+    public Shooter () {}
+
+    public void setPower(){
+        shooterMode = ShooterMode.RAW;
+        power = p;
+    }
+
+    public void spinUp(){
+
+    }
+
 
 }
